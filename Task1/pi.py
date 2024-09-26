7a87ce3b3fb49cbf00e3b19fd94c28056f3bf277
TRISHA9890 <157127861+TRISHA9890@users.noreply.github.com>
2024-09-26 16:15:29 +0530

    Delete Task1 directory

--- Task1/pi.py
+++
@@ -1,9 +0,0 @@
-
-"""store the value 22/7"""
-pi=22/7
-print(pi)
-"""to check the datatype of this variable"""
-print(type(pi))
-
-
- 
--- Task1/speedmeters.py
+++
@@ -1,9 +0,0 @@
-distance=490
-
-time_minutes=7
-
-time_second=time_minutes*60
-speed=distance/time_second
-
-speed=int(speed)
-print("Your speed is",speed,"second")
